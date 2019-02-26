990681e68b6c6f2215f353caad74b067b4ae15f1
0xwangbo <0xwangbo@gmail.com>
2019-02-26 15:54:09 +0800

    Initial Commit

---
+++ Message Forward Demo/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Message Forward Demo
+//
+//  Created by 0xwangbo on 2/26/19.
+//  Copyright © 2019 Bo Wang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
